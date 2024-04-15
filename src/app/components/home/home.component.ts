babda3e5a52c798003e94996408dd64f90252d47
madhurd <madhurd@maveric-systems.com>
2024-04-15 15:12:08 +0530

    css changed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  clockmessage: string = "Check realtime date and time"
+  flagmessage: string = "Discover flags of different countries quickly and easily"
+  colorpicker: string = "Explore a variety of colors and their RGB formats with our interactive color picker including Gradient Color"
+  capitalmessage: string = "Discover the capitals of different countries quickly, efficiently and accurately"
+  currencymessage: string = "Convert currencies easily with real-time exchange rates from around the world included with all countries"
+  calculatormessage: string = "Perform basic and advanced calculations with our user-friendly calculator"
   constructor() { }
 
   ngOnInit(): void {
   }
-  
+
 
 }
